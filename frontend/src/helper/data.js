c05a370541551a40231eda571b643fbc7ff27453
kesh gurung <k_grg@hotmail.com>
2022-02-27 22:50:07 +0000

    deployment

--- frontend/src/helper/data.js
+++ frontend/src/helper/data.js
@@ -33,3 +33,44 @@ export const data = {
     },
   },
 }
+export const productData = app.get('/', (req, res) => {
+  res.json([
+    {
+      id: 1,
+      uid: 'SN0000f43',
+      image: 'https://i.imgur.com/EGHGo3i.png',
+      type: 'S4 Pro',
+      location: 'Site1, Cheddar, UK',
+      status: 'Active',
+    },
+    {
+      id: 2,
+      uid: 'SN0000g96',
+      image: 'https://i.imgur.com/5w7jfqf.png',
+      type: 'S6 Pro',
+      location: 'Site1, Cheddar, UK',
+      status: 'Error',
+    },
+    {
+      id: 3,
+      uid: 'SN0000h56',
+      image: 'https://i.imgur.com/YgIG0cc.png',
+      type: 'Fast',
+      location: 'Vehicle1, Cheddar, UK',
+      status: 'Active',
+    },
+  ])
+})
+
+app.get('/1', (req, res) => {
+  res.json([
+    {
+      id: 1,
+      uid: 'SN0000f43',
+      image: 'https://i.imgur.com/EGHGo3i.png',
+      type: 'S4 Pro',
+      location: 'Site1, Cheddar, UK',
+      status: 'Active',
+    },
+  ])
+})
